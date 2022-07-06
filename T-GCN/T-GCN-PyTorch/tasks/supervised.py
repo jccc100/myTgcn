571d756bcf5757814afc150e5eb16246cb10071f
jc <2428791113@qq.com>
2022-07-06 12:12:56 +0800

    print shape

--- T-GCN/T-GCN-PyTorch/tasks/supervised.py
+++ T-GCN/T-GCN-PyTorch/tasks/supervised.py
@@ -78,8 +78,8 @@ class SupervisedForecastTask(pl.LightningModule):
         predictions = predictions * self.feat_max_val
         predictions2=predictions
         y2=y
-        np.save("prediction.npy",predictions2)
-        np.save("true.npy",y2)
+        np.save("prediction.npy",predictions2.cpu)
+        np.save("true.npy",y2.cpu)
         predictions_shape=predictions.shape
         y = y * self.feat_max_val
         y_shape=y.shape
